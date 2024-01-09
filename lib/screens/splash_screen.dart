b738cb765a5bee5098cc39486c069fab8204ae80
Rhysha Kachari <rhyshakachari2003@gmail.com>
2024-01-09 15:36:25 +0530

    splash screen done

---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:smartcrop/screens/home_screen.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> with SingleTickerProviderStateMixin{
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersive);
+
+    Future.delayed(const Duration(seconds:4),(){
+      Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (_) => const HomeScreen()));
+    });
+
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual,
+    overlays: SystemUiOverlay.values);
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: double.infinity,
+        decoration: BoxDecoration(
+          color: Colors.green,
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const[
+            Icon(Icons.rice_bowl,
+            size: 80,
+            color: Colors.white,),
+            SizedBox(height: 20,),
+            Text('Bhoomi',
+            style: TextStyle(
+              fontStyle: FontStyle.italic,
+              color: Colors.white,
+              fontSize: 40,
+            ),)
+          ],
+        ),
+      ),
+    );
+  }
+}
